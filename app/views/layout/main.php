31b2d0793261cf472a407dd2e34567814203a751
LouKnowsAlready <lourence.l.cabaluna@gmail.com>
2018-10-31 12:02:06 +0800

    replaced jquery (both css and js) CDN with local files version

--- app/views/layout/main.php
+++ app/views/layout/main.php
@@ -3,11 +3,11 @@
 	<head>
 		<title>Bug Tracker</title>
 
-		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+		<link rel="stylesheet" href="/css/jquery.mobile-1.4.5.min.css" />
 		<link rel="stylesheet" href= '/css/style.css' /> 
 		
-		<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-		<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+		<script src="/js/jquery-1.11.1.min.js"></script>
+		<script src="/js/jquery.mobile-1.4.5.min.js"></script>
 		<script src="/js/main.js"></script>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
